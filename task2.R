f5af8963d2a962780ba0d73f9121a32895458542
Morgan <londonspleen@gmail.com>
2023-03-13 12:03:36 -0400

    file

---
+++ task2.R
@@ -0,0 +1,5 @@
+#Draw a line graph: x-axis: date, y-axis: close
+#Draw a bar graph: x-axis: date, y-axis: volume
+#Combine two graphs into one graph so that the stock price and volume data can be displayed together. 
+
+aap1 <- read.csv(aapl.csv)
